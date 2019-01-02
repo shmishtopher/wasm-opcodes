cfa6341d0617b976e65b0f99e85bfab2d6de40a9
Shmish <shmish90@gmail.com>
2019-01-02 14:03:28 -0500

    added num.js

---
+++ esm/index.js
@@ -0,0 +1,12 @@
+export { default as i32 } from './i32.js'
+export { default as i64 } from './i64.js'
+export { default as f32 } from './f32.js'
+export { default as f64 } from './f32.js'
+
+export { default as local } from './local.js'
+export { default as global } from './global.js'
+
+export { default as ctrl } from './ctrl.js'
+export { default as para } from './para.js'
+export { default as mem } from './ctrl.js'
+export { default as num } from './num.js'
---
+++ esm/mem.js
@@ -0,0 +1,33 @@
+export default {
+  i32_LOAD:     0x28,
+  i64_LOAD:     0x29,
+  f32_LOAD:     0x2A,
+  f64_LOAD:     0x2B,
+
+  i32_LOAD8_s:  0x2C,
+  i32_LOAD8_u:  0x2D,
+  i32_LOAD16_s: 0x2E,
+  i32_LOAD16_u: 0x2F,
+
+  i64_LOAD8_s:  0x30,
+  i64_LOAD8_u:  0x31,
+  i64_LOAD16_s: 0x32,
+  i64_LOAD16_u: 0x33,
+  i64_LOAD32_s: 0x34,
+  i64_LOAD32_u: 0x35,
+
+  i32_STORE:    0x36,
+  i64_STORE:    0x37,
+  f32_STORE:    0x38,
+  f64_STORE:    0x39,
+
+  i32_STORE8:   0x3A,
+  i32_STORE16:  0x3B,
+  
+  i64_STORE8:   0x3C,
+  i64_STORE16:  0x3D,
+  i64_STORE32:  0x3E,
+
+  SIZE:         0x3F,
+  GROW:         0x40,
+}
